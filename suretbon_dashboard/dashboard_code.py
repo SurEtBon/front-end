0d3fcb932534a188c23b095928008615d59b4271
Alexis Gourdol <alexis.gourdol@gmail.com>
2024-11-05 10:51:47 +0000

    remove class at the bottom, fixdata sample source file

--- suretbon_dashboard/dashboard_code.py
+++ suretbon_dashboard/dashboard_code.py
@@ -5,7 +5,7 @@ from shapely import wkb
 import folium
 
 
-data =  pd.read_parquet("/home/donatien.konan.pro/code/donat-konan33/de-project-2024/front-end/data/restaurant_matching.parquet")
+data =  pd.read_parquet("../data/restaurant_matching.parquet")
 
 # Convertir la colonne WKB en géométries
 data['osm_geo'] = data['osm_geo'].apply(wkb.loads)
@@ -25,55 +25,3 @@ folium.GeoJson(
 ).add_to(m)
 
 m
-
-class suretbonDashboard:
-    def __init__(self) -> None:
-        pass
-
-    def icone(self):
-        """
-        the Logo of SûrEtBon
-        """
-        with open()
-
-
-    def introduction_page(self):
-        """Layout the views of the dashboard"""
-        st.title("SûrEtBon Dashboard")
-        st.write(
-            """
-       ---
-
-        Restaurant Cleanliness and Customer Rating Evaluation
-        Application Overview
-        This Streamlit application is designed to help users evaluate restaurants based on two essential criteria:
-        sanitary cleanliness and customer satisfaction. Providing a user-friendly interface, the app allows users to search for,
-        review, and rate various dining establishments.
-
-        ---
-        Key Features
-        ---
-
-        Cleanliness Evaluation: Users can review information on the sanitary cleanliness of restaurants, based on established standards and inspections.
-        Customer Ratings: The app gathers and displays ratings from customers, providing an overview of customer satisfaction for each restaurant.
-        Search Filters: Users can filter results by cuisine type, location, or cleanliness score, making it easy to find a restaurant that meets their expectations.
-        Intuitive Interface: With Streamlit’s simple and interactive design, users can easily navigate through the information,
-        access analysis graphs, and submit their own ratings.
-
-        ---
-
-        Application Objective
-        ---
-        The primary objective of this application is to promote healthy and
-        transparent dining practices by providing customers with reliable information to make informed restaurant choices.
-        By combining cleanliness evaluations with customer ratings, the app encourages restaurant owners to uphold high
-        standards of cleanliness and service.
-        """
-        )
-
-
-
-
-if __name__ == "__main__":
-    dashboard = suretbonDashboard()
-    dashboard.introduction_page()
