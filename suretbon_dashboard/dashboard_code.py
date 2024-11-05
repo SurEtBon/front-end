20a15de2f5bb52a19f13283aa0f8b5b71f1a6958
Alexis Gourdol <alexis.gourdol@gmail.com>
2024-11-05 11:27:37 +0000

    basic aspp with folium plotting sample values

--- suretbon_dashboard/dashboard_code.py
+++ suretbon_dashboard/dashboard_code.py
@@ -1,27 +1,60 @@
-import streamlit as st
-import pandas as pd
-import geopandas as gpd
-from shapely import wkb
 import folium
+import geopandas as gpd
+import pandas as pd
+import streamlit as st
+
+from streamlit_folium import st_folium
+from shapely import wkb
 
 
-data =  pd.read_parquet("../data/restaurant_matching.parquet")
+# source data
+data =  pd.read_parquet("data/restaurant_matching_sample.parquet")
+col1, col2 = st.columns(2)
 
-# Convertir la colonne WKB en géométries
+with col1:
+    st.write("sourced data: data types")
+    st.write(data.dtypes.tolist())
+
+with col2:
+    st.dataframe(data)
+
+
+
+# convert 'osm_geo' into valid geo data
 data['osm_geo'] = data['osm_geo'].apply(wkb.loads)
-# Convertir ensuite en GeoDataFrame
+
+# convert to GeoDataFrame
 gdf = gpd.GeoDataFrame(data, geometry='osm_geo', crs="EPSG:4326")
-gdf = gdf[['osm_name', 'osm_type',
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write("converted geodataframe : data types")
+    st.write(gdf.dtypes.tolist())
+
+with col2:
+    st.dataframe(gdf)
+
+columns_to_keep = ['osm_name', 'osm_type',
        'osm_clean_name', 'osm_geo',
        'cs_app_libelle_activite_etablissement',
-       'levenshtein_distance']]
+       'levenshtein_distance']
+st.write(f"keep only these columns: {columns_to_keep}")
+gdf = gdf[columns_to_keep]
 
-gdf = gdf[gdf['levenshtein_distance'] <= 3]
+mask = gdf['levenshtein_distance'] <= 3
+st.write(f"keep only these rows with levenshtein_distance <= 3")
+gdf = gdf[mask]
 
-m = folium.Map(location=[gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()])
+m = folium.Map(
+    location=[48.8566, 2.3522],
+    zoom_start=12
+    )
+
 folium.GeoJson(
     gdf,
     tooltip=folium.features.GeoJsonTooltip(fields=['osm_clean_name'])
 ).add_to(m)
 
-m
+st_data = st_folium(m, width=725)
+
