542cd1193a6d943a4fbae4dee0cfd46ef5348dbe
Alexis Gourdol <alexis.gourdol@gmail.com>
2024-11-06 16:15:46 +0100

    Map with side bar display of info (#4)
    * attempt to solve stramlit cloud deploy issue : /mount/src/front-end /mount/src/front-end Current Python version (3.12.7) is not allowed by the project (3.12.4).
    
    * remove lock file to solve pyproject.toml changed significantly since poetry.lock was last generated. Run  to fix the lock file.
    
    * update data source from intermediate to mart table
    
    * Added Dev Container Folder
    
    * remove devcontainer
    
    * remove unused imports
    
    * refresh map every 1H, remove dataframes from app, remove commented  geoloc cols that made the code bug in previous iterations
    
    * remove writes and write SQL query in multiline
    
    * trying to get last clicked item on the map
    
    * checking if folium static solves marker icon not appearing issue
    
    * folium_static shows proper icon but st_data['last_clicked'] DeltaGenerator' object is not subscriptable
    
    * back to st_folium
    
    * working icons and click data
    
    * factored out load_data into a utils.py, added side bar to display restaurant info

--- suretbon_dashboard/dashboard_code.py
+++ suretbon_dashboard/dashboard_code.py
@@ -2,130 +2,84 @@ import folium
 import geopandas as gpd
 import pandas as pd
 import streamlit as st
-import os
-import logging
-
-from google.oauth2 import service_account
-from google.cloud import bigquery
-from google.auth.exceptions import DefaultCredentialsError
 
 from streamlit_folium import st_folium
 from shapely import wkb
-from shapely import Geometry
-from shapely import to_wkt
 
-try:
-    # Create API client.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"]
-    )
-    client = bigquery.Client(credentials=credentials)
-    logging.info("Big Query client successfully created")
+from utils import load_data
+from utils import write_clicked_restaurant_data
 
-    # Perform query.
-    # Uses st.cache_data to only rerun when the query changes or after 10 min.
-    @st.cache_data(ttl=600)
-    def run_query(query):
-        query_job = client.query(query)
-        rows_raw = query_job.result()
-        # Convert to list of dicts. Required for st.cache_data to hash the return value.
-        rows = [dict(row) for row in rows_raw]
-        return rows
+st.set_page_config(layout="wide")
 
-    query = """SELECT * FROM `algebraic-link-440513-f9.intermediate.int_join_restaurants` LIMIT 1000"""
-    results = run_query(query)
+query = """
+    SELECT *
+    FROM `algebraic-link-440513-f9.mart.restaurants_final_matching`
+    LIMIT 1000
+    """
 
-    df = pd.DataFrame(results)
+df = load_data(query)
 
-except DefaultCredentialsError as e:
-    logging.error(
-        f"Error: {e}. Check environment variables and service account credentials."
-    )
-    logging.info("Loading data from local `parquet` file..")
-    df = pd.read_parquet("data/restaurant_matching_sample.parquet")
+# convert geospatial data into type that works with pandas
+df["geopandas_osm"] = df["geopandas_osm"].apply(wkb.loads)
 
-col1, col2 = st.columns(2)
+# convert to GeoDataFrame
+gdf = gpd.GeoDataFrame(df, geometry="geopandas_osm", crs="EPSG:4326")
 
-with col1:
-    st.write("sourced data: data types")
-    st.write(df.dtypes.tolist())
-
-with col2:
-    st.dataframe(df)
-
-col1, col2 = st.columns(2)
-
-with col1:
-    st.write("converted geodataframe : data types")
-    df["geopandas_osm"] = df["geopandas_osm"].apply(wkb.loads)
-    # convert to GeoDataFrame
-    gdf = gpd.GeoDataFrame(df, geometry="geopandas_osm", crs="EPSG:4326")
-    st.write(gdf.dtypes.tolist())
-
-with col2:
-    st.dataframe(gdf)
-
+# filter out some columns
 columns_to_keep = [
     "osm_name",
     "type",
     "osm_clean_name",
-    #"geo_osm",
     "geopandas_osm",
     "osm_siret",
     "alimconfiance_name",
     "alimconfiance_clean_name",
     "alimconfiance_siret",
-    #"geo_alimconfiance",
-    #"geopandas_alimconfiance",
     "distance_name_label",
     "stars",
     "synthese_eval_sanit",
     "app_code_synthese_eval_sanit",
 ]
-st.write(f"keep only these columns: {columns_to_keep}")
 gdf = gdf[columns_to_keep]
 
+# keep only these rows with levenshtein_distance <= 3
 mask = gdf["distance_name_label"] <= 3
-st.write(f"keep only these rows with levenshtein_distance <= 3")
 gdf = gdf[mask]
 
+address = st.text_input("Adresse")
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12, tiles="cartodb positron")
+
 
 folium.GeoJson(
     gdf,
-    tooltip=folium.features.GeoJsonTooltip(fields=["osm_clean_name"])
+    tooltip=folium.features.GeoJsonTooltip(fields=["osm_clean_name"]),
 ).add_to(m)
 
 st_data = st_folium(m, width=725)
+st.write(dict(st_data))
+
+with st.sidebar:
+    st_data = dict(st_data)
+    display_data = st_data["last_active_drawing"]["properties"]
+
+    name = display_data["osm_name"]
+    st.write("**Name**")
+    name = display_data["osm_name"]
+    st.write(name)
+    st.divider()
 
 
-"""
-tile design options : https://leaflet-extras.github.io/leaflet-providers/preview/
-
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
-
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
+    stars = display_data["stars"]
+    st.write("**Stars**")
+    st.write(stars)
+    st.divider()
 
 
-Provider names for leaflet-providers.js
-Stadia.OSMBright
-Plain JavaScript:
-var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
+    eval = display_data["app_code_synthese_eval_sanit"]
+    st.write("**Evaluation**")
+    st.write(eval)
+    st.divider()
 
-
-"""
+    synthese = display_data["synthese_eval_sanit"]
+    st.write("**Synthese**")
+    st.write(synthese)
