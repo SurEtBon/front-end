d52c7066ffd681cd3ecfc92603798694a44515f5
Alexis Gourdol <alexis.gourdol@gmail.com>
2024-11-06 16:56:02 +0000

    with colours, nb_inspections and date_inspection

--- suretbon_dashboard/dashboard_code.py
+++ suretbon_dashboard/dashboard_code.py
@@ -21,6 +21,7 @@ df = load_data(query)
 
 # convert geospatial data into type that works with pandas
 df["geopandas_osm"] = df["geopandas_osm"].apply(wkb.loads)
+df["date_inspection"] = df["date_inspection"].apply(str)
 
 # convert to GeoDataFrame
 gdf = gpd.GeoDataFrame(df, geometry="geopandas_osm", crs="EPSG:4326")
@@ -33,6 +34,9 @@ columns_to_keep = [
     "distance_name_label",
     "synthese_eval_sanit",
     "app_code_synthese_eval_sanit",
+    "full_address",
+    "nb_inspections",
+    "date_inspection",
 ]
 gdf = gdf[columns_to_keep]
 
@@ -43,10 +47,25 @@ gdf = gdf[mask]
 address = st.text_input("Adresse")
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12, tiles="cartodb positron")
 
+def get_feature_style(feature):
+    eval_code = feature["properties"]["app_code_synthese_eval_sanit"]  # Access the app_code_synthese_eval_sanit value
+    color_map = {
+        1: "green",
+        2: "blue",
+        3: "black",
+        4: "red"
+    }
+    return {
+        "color": color_map.get(eval_code, "gray"),  # Default to gray if code not matched
+        "weight": 2,
+        "fillOpacity": 0.6
+    }
 
 folium.GeoJson(
     gdf,
-    tooltip=folium.features.GeoJsonTooltip(fields=["osm_clean_name"]),
+    tooltip=folium.features.GeoJsonTooltip(fields=["osm_name"]),
+    marker=folium.Circle(radius=300), #, fill_color="orange", fill_opacity=0.4, color="black", weight=1),
+    style_function=get_feature_style
 ).add_to(m)
 
 st_data = st_folium(m, width=725)
@@ -58,7 +77,7 @@ with st.sidebar:
 
     name = display_data["osm_name"]
     full_address = display_data["full_address"]
-    st.write("**Name**")
+    st.write("**Nom de l'établissement**")
     st.write(name)
     st.write("**Adresse**")
     st.write(full_address)
@@ -68,8 +87,13 @@ with st.sidebar:
     eval = display_data["app_code_synthese_eval_sanit"]
     synthese = display_data["synthese_eval_sanit"]
     st.write("**Evaluation**")
-    st.write(eval + " - " + synthese)
+    st.write(str(eval) + " - " + synthese)
     st.write(CODE_DESCRIPTION[eval])
+    st.write(f"Date de la dernière inspection : {display_data['date_inspection']}")
+    st.write(f"Nombre d'inspection(s) : {display_data['nb_inspections']}")
+
+
+    st.write(st_data)
 
 
 
